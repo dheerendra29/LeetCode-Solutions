7fd2487e6bf4103063a99bd4e127e859de01c20b
Dheerendra Gururani <dheerendragururani29@gmail.com>
2026-08-03 08:57:58 +0530

    Time: 20 ms (72.63%) | Memory: 136.1 MB (83.46%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+
+        vector<int> dp(n + 1, 0);
+
+        // dp[i] = maximum score difference
+        // current player can achieve from i onwards
+
+        for (int i = n - 1; i >= 0; i--) {
+            int sum = 0;
+            dp[i] = INT_MIN;
+
+            for (int j = i; j < min(n, i + 3); j++) {
+                sum += stoneValue[j];
+
+                dp[i] = max(dp[i], sum - dp[j + 1]);
+            }
+        }
+
+        if (dp[0] > 0)
+            return "Alice";
+        else if (dp[0] < 0)
+            return "Bob";
+        else
+            return "Tie";
+    }
+};
