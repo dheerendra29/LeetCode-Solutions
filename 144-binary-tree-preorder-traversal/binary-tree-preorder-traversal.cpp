e12a75679f400a82219ef8504aff31f9b1f659b6
Dheerendra Gururani <dheerendragururani29@gmail.com>
2026-08-04 14:01:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (15.63%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> ans;
+
+    void solve(TreeNode* root) {
+        if (root == NULL)
+            return;
+
+        ans.push_back(root->val);
+        solve(root->left);
+        solve(root->right);
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        solve(root);
+        return ans;
+    }
+};
